b9e7a001bd6ddc98de0c45bc9c275085d776ea7b
Denis Sitek <ugfan13@gmail.com>
2020-11-22 12:25:11 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9990;
+        int miles = 5;
+        int milesammount = ticket * miles / 100;
+        System.out.println(milesammount);
+    }
+
+
+}
